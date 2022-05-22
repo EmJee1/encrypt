7b8f0c15f339968edadcbdac2ab4199d98e9c9a7
EmJee <mart-janroeleveld@outlook.com>
2022-05-22 14:24:46 +0200

    Add jsdoc comment to readFileSafe

--- src/filesystem.ts
+++ src/filesystem.ts
@@ -1,7 +1,15 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
 
-export const readFileSafe = async (file: unknown): Promise<string> => {
+/**
+ * @name readFileSafe
+ * @summary reads a file in utf-8 encoding after checking the parameter type and if the file exists
+ * @param {string} file the filename or path to the file
+ * @returns {string} the contents of the file in utf-8 encoding
+ * @throws {TypeError} string type required
+ * @throws {TypeError} file needs to exist
+ */
+export const readFileSafe = async (file: string): Promise<string> => {
   if (typeof file !== 'string') {
     throw new TypeError(`File should be a string, received '${typeof file}'`)
   }
