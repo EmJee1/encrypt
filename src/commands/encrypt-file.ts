114a951667fc7fcd14be69ce6c7e1439875bbc8e
EmJee <mart-janroeleveld@outlook.com>
2022-05-22 18:29:55 +0200

    Extract command logic into separate function

--- src/commands/encrypt-file.ts
+++ src/commands/encrypt-file.ts
@@ -1,26 +1,38 @@
 import path from 'node:path'
 import fs from 'node:fs/promises'
-import { Command } from 'commander'
+import { Command, program } from 'commander'
 import { encryptAes } from '../aes-encryption'
 import { logJson } from '../terminal'
+import { readFileSafe } from '../filesystem'
+
+const handleEncryptFile = async (
+  filepath: string,
+  encryptedFilename?: string
+) => {
+  const fileContents = await readFileSafe(filepath)
+  const encryptedPayload = encryptAes(fileContents)
+
+  if (encryptedFilename) {
+    const resolvedPathToWrite = path.resolve(encryptedFilename)
+    await fs.writeFile(resolvedPathToWrite, encryptedPayload.encrypted, 'utf-8')
+    logJson({ iv: encryptedPayload.iv, key: encryptedPayload.key })
+    return
+  }
+
+  logJson(encryptedPayload)
+}
 
 const encryptFile = new Command('encrypt-file')
   .description('Encrypt a file')
   .argument('<filepath>', 'path to the file to be encrypted')
   .argument('[filepath]', 'path to the encrypted file')
   .action(async (filepath: string, encryptedFilename?: string) => {
-    const resolvedPath = path.resolve(filepath)
-    const fileContents = await fs.readFile(resolvedPath, 'utf-8')
-    const encrypted = encryptAes(fileContents)
-
-    if (encryptedFilename) {
-      const resolvedPathToWrite = path.resolve(encryptedFilename)
-      await fs.writeFile(resolvedPathToWrite, encrypted.encrypted, 'utf-8')
-      logJson({ iv: encrypted.iv, key: encrypted.key })
-      return
+    try {
+      await handleEncryptFile(filepath, encryptedFilename)
+    } catch (err) {
+      // TODO: implement error logger handler
+      return program.error('')
     }
-
-    logJson(encrypted)
   })
 
 export default encryptFile
