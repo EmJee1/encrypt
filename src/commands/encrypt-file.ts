5b51fb0b55c610c48647904769eb0773db33373a
EmJee <mart-janroeleveld@outlook.com>
2022-05-22 14:19:12 +0200

    Write encrypted file for encrypt-file option

--- src/commands/encrypt-file.ts
+++ src/commands/encrypt-file.ts
@@ -7,10 +7,19 @@ import { logJson } from '../terminal'
 const encryptFile = new Command('encrypt-file')
   .description('Encrypt a file')
   .argument('<filepath>', 'path to the file to be encrypted')
+  .argument('[filepath]', 'path to the encrypted file')
   .action(async (filepath: string, encryptedFilename?: string) => {
     const resolvedPath = path.resolve(filepath)
     const fileContents = await fs.readFile(resolvedPath, 'utf-8')
     const encrypted = encryptAes(fileContents)
+
+    if (encryptedFilename) {
+      const resolvedPathToWrite = path.resolve(encryptedFilename)
+      await fs.writeFile(resolvedPathToWrite, encrypted.encrypted, 'utf-8')
+      logJson({ iv: encrypted.iv, key: encrypted.key })
+      return
+    }
+
     logJson(encrypted)
   })
 
